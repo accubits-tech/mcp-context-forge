a08e324bbc303fa0bd645568814bb1815eaf5759
RAHUL A R <rahular101@gmail.com>
2025-11-19 11:58:43 +0530

    fixed the deployment issues

---
+++ mcpgateway/alembic/versions/9a79c73f2b78_merge_gateway_url_constraint_migrations.py
@@ -0,0 +1,28 @@
+"""merge gateway url constraint migrations
+
+Revision ID: 9a79c73f2b78
+Revises: f3a3a3d901b8, i3j4k5l6m7n8
+Create Date: 2025-11-19 11:47:58.507673
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '9a79c73f2b78'
+down_revision: Union[str, Sequence[str], None] = ('f3a3a3d901b8', 'i3j4k5l6m7n8')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
